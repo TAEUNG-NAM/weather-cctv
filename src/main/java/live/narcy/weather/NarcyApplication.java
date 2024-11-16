7d60e17d16208ce07a7d9bef919050208dccca70
TAEUNG-NAM <106743597+TAEUNG-NAM@users.noreply.github.com>
2024-11-16 21:44:44 +0900

    Untrack files in .gitignore

---
+++ src/main/java/live/narcy/weather/NarcyApplication.java
@@ -0,0 +1,13 @@
+package live.narcy.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarcyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarcyApplication.class, args);
+    }
+
+}
